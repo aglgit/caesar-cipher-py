a86ce57841c0639a359de4922b8f10df802842f6
Andreas L <aglgit@users.noreply.github.com>
2025-01-08 13:41:57 +0100

    Refactoring

--- cipher.py
+++ cipher.py
@@ -8,28 +8,35 @@ class CaesarCipher:
         self.upper_norwegian_map = {198: 26, 216: 27, 197: 28}
         self.upper_norwegian_inv_map = {v: k for k, v in self.upper_norwegian_map.items()}
 
+    def encode(self, string):
+        char_array = list(string)
+        char_array = [self.encode_char(c, self.cipher) for c in char_array]
+        return "".join(char_array)
+
+    def decode(self, string):
+        char_array = list(string)
+        char_array = [self.encode_char(c, -self.cipher) for c in char_array]
+        return "".join(char_array)
+
     def encode_char(self, char, cipher):
         code_point = ord(char)
         if 65 <= code_point <= 90:
-            index = self.map_lower_unicode_to_index(cipher, code_point)
+            index = self.map_unicode_to_index(cipher, code_point, 65)
             return self.map_index_to_lower_char(index)
         elif 97 <= code_point <= 122:
-            index = self.map_upper_unicode_to_index(cipher, code_point)
+            index = self.map_unicode_to_index(cipher, code_point, 97)
             return self.map_index_to_upper_char(index)
         elif code_point in self.lower_norwegian_map:
-            index = self.map_lower_norwegian_unicode_to_index(cipher, code_point)
+            index = self.map_norwegian_unicode_to_index(cipher, code_point)
             return self.map_index_to_upper_char(index)
         elif code_point in self.upper_norwegian_map:
-            index = self.map_upper_norwegian_unicode_to_index(cipher, code_point)
+            index = self.map_norwegian_unicode_to_index(cipher, code_point)
             return self.map_index_to_lower_char(index)
         else:
             return chr(code_point)
 
-    def map_lower_unicode_to_index(self, cipher, code_point):
-        return (code_point - 65 + cipher) % self.num_alphabet
-
-    def map_upper_unicode_to_index(self, cipher, code_point):
-        return (code_point - 97 + cipher) % self.num_alphabet
+    def map_unicode_to_index(self, cipher, code_point, lower_bound):
+        return (code_point - lower_bound + cipher) % self.num_alphabet
 
     def map_index_to_lower_char(self, index):
         if index in self.upper_norwegian_inv_map:
@@ -43,21 +50,11 @@ class CaesarCipher:
         else:
             return chr(index + 97)
 
-    def map_lower_norwegian_unicode_to_index(self, cipher, code_point):
-        return (self.lower_norwegian_map.get(code_point) + cipher) % self.num_alphabet
-
-    def map_upper_norwegian_unicode_to_index(self, cipher, code_point):
-        return (self.upper_norwegian_map.get(code_point) + cipher) % self.num_alphabet
-
-    def encode(self, string):
-        char_array = list(string)
-        char_array = [self.encode_char(c, self.cipher) for c in char_array]
-        return "".join(char_array)
-
-    def decode(self, string):
-        char_array = list(string)
-        char_array = [self.encode_char(c, -self.cipher) for c in char_array]
-        return "".join(char_array)
+    def map_norwegian_unicode_to_index(self, cipher, code_point):
+        if code_point in self.upper_norwegian_map:
+            return (self.upper_norwegian_map.get(code_point) + cipher) % self.num_alphabet
+        if code_point in self.lower_norwegian_map:
+            return (self.lower_norwegian_map.get(code_point) + cipher) % self.num_alphabet
 
 
 if __name__ == "__main__":
