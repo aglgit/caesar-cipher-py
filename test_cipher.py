2b04f3960541576c00b2e796484361f8ac6065a2
Andreas L <aglgit@users.noreply.github.com>
2025-01-08 12:55:16 +0100

    Add unit tests

---
+++ test_cipher.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from cipher import CaesarCipher
+
+
+class TestCaesarCipher(TestCase):
+    def test_encode(self):
+        cipher = CaesarCipher(3)
+        text = "abcdef xyzæøå ABCDEFXYZ ÆØÅ"
+        self.assertEquals("defghi æøåabc DEFGHIÆØÅ ABC", cipher.encode(text))
+
+    def test_decode(self):
+        cipher = CaesarCipher(3)
+        text = "defghi æøåabc DEFGHIÆØÅ ABC"
+        self.assertEquals("abcdef xyzæøå ABCDEFXYZ ÆØÅ", cipher.decode(text))
+
+    def test_encode_then_decode(self):
+        cipher = CaesarCipher(3)
+        text = "defghi æøåabc DEFGHIÆØÅ ABC"
+        self.assertEquals(text, cipher.decode(cipher.encode(text)))
